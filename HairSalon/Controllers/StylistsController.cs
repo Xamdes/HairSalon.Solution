3503c1ac65a3cd68817cbe5c9a0c81ff635ba491
Steven Colburn <stevencolburn@xamdes.com>
2018-07-13 12:44:34 -0700

    add stylist view

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string description, DateTime date)
+    {
+      Stylist newItem = new Stylist (description,date);
+      newItem.Save();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult Details(int id)
+    {
+      Stylist Stylist = Stylist.Find(id);
+      return View(Stylist);
+    }
+
+    [HttpGet("/stylists/{id}/delete")]
+    public ActionResult Delete(int id)
+    {
+        Stylist.DeleteId(id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost("/stylists/delete")]
+    public ActionResult DeleteAll()
+    {
+      Stylist.DeleteAll(true);
+      return View();
+    }
+  }
+
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,21 +4,88 @@ using System.Collections.Generic;
 using System.Linq;
 using MySql.Data.MySqlClient;
 
-
-
 namespace HairSalon.Models
 {
   public class Stylist
   {
+    private static string tableName = _stylists;
     private string _name;
     private int _id;
-    private List<Client> clients;
+    private List<Client> _clients;
 
     public Stylist(string name,int id = -1)
     {
       _name = name;
       _id = id;
-      clients = new List<Client>(){};
+      _clients = new List<Client>(){};
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public void Save()
+    {
+      string columns = "id,name";
+      List<string> valueNames = new List<string>(){"@Id","@Name"};
+      List<Object> values = new List<Object>(){_id,_name};
+      DB.SaveToTable(_tableName,columns,valueNames,values);
+      _id = DB.LastInsertId(_tableName);
+    }
+
+    public static List<Stylist> GetAll(string orderBy = "id", string order = "ASC")
+    {
+      List<Stylist> stylists = new List<Stylist>(){};
+      DB.OpenConnection();
+      DB.SetCommand(@"SELECT * FROM stylists ORDER BY "+orderBy+" "+order+";");
+      MySqlDataReader rdr = DB.ReadSqlCommand();
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Stylist newStylist = new Stylist(id,name);
+        stylists.Add(newStylist);
+      }
+      DB.CloseConnection();
+      return stylists;
+    }
+
+    public static Stylist Find(int id)
+    {
+      int stylistId = -1;
+      string name = "";
+      DB.OpenConnection();
+      DB.SetCommand(@"SELECT * FROM stylists WHERE id=@thisId;");
+      DB.AddParameter("@thisId",id);
+      MySqlDataReader rdr = DB.ReadSqlCommand();
+      while(rdr.Read())
+      {
+        stylistId = rdr.GetInt32(0);
+        name = rdr.GetString(1);
+      }
+      DB.CloseConnection();
+      return (new Stylist(stylistId,name));
+    }
+
+    public static void DeleteAll(bool saveUniqueIds = true)
+    {
+      DB.ClearTable(_tableName,saveUniqueIds);
+    }
+
+    public static void DeleteId(int deleteId)
+    {
+      DB.DeleteById(_tableName,deleteId);
+    }
+
+    public void Delete()
+    {
+      DB.DeleteById(_tableName,_id);
     }
 
   }
